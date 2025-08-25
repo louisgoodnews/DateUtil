7431984b4a66c0c071453069020e55b35d9b84c3
Louis Goodnews <louisgoodnews95@gmail.com>
2025-08-25 09:32:27 +0200

    Updated a few things.

--- src/DateUtil/__init__.py
+++ src/DateUtil/__init__.py
@@ -1,5 +1,12 @@
-from typing import Final, List
+"""
+Author: Louis Goodnews
+Date: 2025-08-19
+"""
+
+from typing import Final, List, Literal
 
 from .core.core import DateFormat, DateUtil
 
 __all__: Final[List[str]] = ["DateFormat", "DateUtil"]
+
+__version__: Final[Literal["0.1.0"]] = "0.1.0"
